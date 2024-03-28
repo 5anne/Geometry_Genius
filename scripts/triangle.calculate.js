677b09a4672226359cf2d87eb1d5ee6756c78c78
5anne <tahaminaanne@gmail.com>
2024-03-28 23:40:12 +0600

    first commit

---
+++ scripts/triangle.calculate.js
@@ -0,0 +1,18 @@
+function calculateTriangleArea(){
+    const baseInput = document.getElementById('base');
+    const base = parseFloat(baseInput.value);
+    // console.log(baseInput.value);
+    // console.log(typeof parseFloat(baseInput.value));
+
+    const heightInput = document.getElementById('height');
+    const height = parseFloat(heightInput.value);
+    // console.log(heightInput.value);
+    // console.log(typeof parseFloat(heightInput.value));
+
+    const area = 0.5 * base * height;
+    console.log('Area of the tiangle is:', area);
+
+    const triangleArea = document.getElementById('triangle-area');
+    triangleArea.innerText = area;
+}
+
